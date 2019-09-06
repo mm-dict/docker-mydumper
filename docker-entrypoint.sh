1fd32d71b40fe7a5cc394997d5860085f649ed59
Kristof Keppens <kkeppens@gmail.com>
2019-09-06 09:43:20 +0200

    add some extra log information

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,8 +6,8 @@ echo "Starting mydumper for database ${MYSQL_DATABASE}"
 
 if [ "$MYDUMPER_DAEMON" = "true" ]
 then
-	cmd="mydumper --daemon -h "${MYSQL_HOST}" -u "${MYSQL_USER}" -p "${MYSQL_PASSWORD}" -B "${MYSQL_DATABASE}" -o /backup/export-$(date '+%Y%m%d-%H%M%S') -c"
+	cmd="mydumper -v 3 --daemon -h "${MYSQL_HOST}" -u "${MYSQL_USER}" -p "${MYSQL_PASSWORD}" -B "${MYSQL_DATABASE}" -o /backup -c"
 else
-	cmd="mydumper -h "${MYSQL_HOST}" -u "${MYSQL_USER}" -p "${MYSQL_PASSWORD}" -B "${MYSQL_DATABASE}" -o /backup/export-$(date '+%Y%m%d-%H%M%S') -c"
+	cmd="mydumper -v 3 -h "${MYSQL_HOST}" -u "${MYSQL_USER}" -p "${MYSQL_PASSWORD}" -B "${MYSQL_DATABASE}" -o /backup/export-$(date '+%Y%m%d-%H%M%S') -c"
 fi
 exec $cmd
